376f5ad9b8c09ee4363222dbe317342bd67ce13b
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2014-08-11 16:59:05 +0200

    full controls / ready for Germany (sebkha chott)

--- sebkha.py
+++ sebkha.py
@@ -1,4 +1,4 @@
-import oscwebgui
+import oschtmlgui
 from os import path as os_path
 
 #Ports osc des clients NonMixer
@@ -14,7 +14,7 @@ nmPorts = {
     'Acoustics':'SCSon:6674',
     'MonitorsDag':'SCSon:6675',
     'MonitorsJeannot':'SCSon:6676',
-    'MonitorsOrl':'SCSon:6677',
+    'MonitorsORL':'SCSon:6677',
     'Mains':'SCSon:6678',
 }
 
@@ -28,6 +28,7 @@ pathPatch = {
     'MxSynths':['/strip/MxSynths',nmPorts['MainMix']],
     'MxDrums':['/strip/MxDrums',nmPorts['MainMix']],
     'Vocals':['/strip/Vocals',nmPorts['MainMix']],
+    'Acoustics':['/strip/Acoustics',nmPorts['MainMix']],
 
         #Drums
         'Kick':['/strip/Kick',nmPorts['Drums']],
@@ -43,12 +44,22 @@ pathPatch = {
     
         #Basses
         'Bass_ORL':['/strip/Bass_ORL',nmPorts['Basses']],
-        'FX_ORL':['/strip/FX_ORL',nmPorts['Basses']],
+        'Oct_Bass_ORL':['/strip/Oct_Bass_ORL',nmPorts['Basses']],
+        'FX_Bass_ORL':['/strip/FX_Bass_ORL',nmPorts['Basses']],
         'Bass_Dag':['/strip/Bass_Dag',nmPorts['Basses']],
+        'Oct_Bass_Dag':['/strip/Oct_Bass_Dag',nmPorts['Basses']],
+        'FX_Bass_Dag':['/strip/FX_Bass_Dag',nmPorts['Basses']],
     
         #Guitars
         'Guitar_ORL':['/strip/Guitar_ORL',nmPorts['Guitars']],
+        'FX_Gtr_ORL_1':['/strip/FX_Gtr_ORL_1',nmPorts['Guitars']],
+        'FX_Gtr_ORL_2':['/strip/FX_Gtr_ORL_2',nmPorts['Guitars']],
+        'FX_Gtr_ORL':['/strip/FX_Gtr_ORL',nmPorts['Guitars']],
         'Guitar_Dag':['/strip/Guitar_Dag',nmPorts['Guitars']],
+        'FX_Gtr_Dag_1':['/strip/FX_Gtr_Dag_1',nmPorts['Guitars']],
+        'FX_Gtr_Dag_2':['/strip/FX_Gtr_Dag_2',nmPorts['Guitars']],
+        'FX_Gtr_Dag':['/strip/FX_Gtr_Dag',nmPorts['Guitars']],
+        'Scape_Gtr_Dag':['/strip/Scape_Gtr_Dag',nmPorts['Guitars']],
     
         #MxSynths
         'MxBass':['/strip/MxBass',nmPorts['MxSynths']],
@@ -63,10 +74,22 @@ pathPatch = {
         'MxCymbs':['/strip/MxCymbs',nmPorts['MxDrums']],
         'MxCont':['/strip/MxCont',nmPorts['MxDrums']],
         'MxSamples':['/strip/MxSamples',nmPorts['MxDrums']],
+        
+        #Acoustics
+        'PianoToy':['/strip/PianoToy',nmPorts['Acoustics']],
+        'Cymbalum':['/strip/Cymbalum',nmPorts['Acoustics']],
+        'GuitarAc':['/strip/GuitarAc',nmPorts['Acoustics']],
     
         #Vocals
         'Vx_ORL':['/strip/Vx_ORL',nmPorts['Vocals']],
+        'FX_Vx_ORL_1':['/strip/FX_Vx_ORL_1',nmPorts['Vocals']],
+        'FX_Vx_ORL_2':['/strip/FX_Vx_ORL_2',nmPorts['Vocals']],
+        'FX_Vx_ORL':['/strip/FX_Vx_ORL',nmPorts['Vocals']],
         'Vx_Dag':['/strip/Vx_Dag',nmPorts['Vocals']],
+        'FX_Vx_Dag_1':['/strip/FX_Vx_Dag_1',nmPorts['Vocals']],
+        'FX_Vx_Dag_2':['/strip/FX_Vx_Dag_2',nmPorts['Vocals']],
+        'FX_Vx_Dag':['/strip/FX_Vx_Dag',nmPorts['Vocals']],
+        'Scape_Vx_Dag':['/strip/Scape_Vx_Dag',nmPorts['Vocals']],
         'Vx_Jeannot':['/strip/Vx_Jeannot',nmPorts['Vocals']],
         
     #Mains
@@ -76,29 +99,37 @@ pathPatch = {
     'MonitorsDag':['/strip/MonitorsDag',nmPorts['Mains']],
     
         #MonitorsDag
-        'mdMainMix':['/strip/MainMix',nmPorts['MonitorsDag']],
         'mdDrums':['/strip/Drums',nmPorts['MonitorsDag']],
+        'mdBasses':['/strip/Basses',nmPorts['MonitorsDag']],
+        'mdGuitars':['/strip/Guitars',nmPorts['MonitorsDag']],
+        'mdMxDrums':['/strip/MxDrums',nmPorts['MonitorsDag']],
+        'mdMxSynths':['/strip/MxSynths',nmPorts['MonitorsDag']],
+        'mdVocals':['/strip/Vocals',nmPorts['MonitorsDag']],
         'mdBassDag':['/strip/BassDag',nmPorts['MonitorsDag']],
         'mdGuitarDag':['/strip/GuitarDag',nmPorts['MonitorsDag']],
         'mdVoixDag':['/strip/VoixDag',nmPorts['MonitorsDag']],
         'mdMetronome':['/strip/Metronome',nmPorts['MonitorsDag']],
         
         #MonitorsJeannot
-        'mjMainMix':['/strip/MainMix',nmPorts['MonitorsJeannot']],
         'mjDrums':['/strip/Drums',nmPorts['MonitorsJeannot']],
-        'mjVocals':['/strip/Vocals',nmPorts['MonitorsJeannot']],
-        'mjGuitars':['/strip/Guitars',nmPorts['MonitorsJeannot']],
         'mjBasses':['/strip/Basses',nmPorts['MonitorsJeannot']],
+        'mjGuitars':['/strip/Guitars',nmPorts['MonitorsJeannot']],
+        'mjMxDrums':['/strip/MxDrums',nmPorts['MonitorsJeannot']],
+        'mjMxSynths':['/strip/MxSynths',nmPorts['MonitorsJeannot']],
+        'mjVocals':['/strip/Vocals',nmPorts['MonitorsJeannot']],
         'mjMetronome':['/strip/Metronome',nmPorts['MonitorsJeannot']],
 
-        #MonitorsOrl
-        'moMainMix':['/strip/MainMix',nmPorts['MonitorsOrl']],
-        'moDrums':['/strip/Drums',nmPorts['MonitorsOrl']],
-        'moBassORL':['/strip/BassORL',nmPorts['MonitorsOrl']],
-        'moGuitarORL':['/strip/GuitarORL',nmPorts['MonitorsOrl']],
-        'moVxORL':['/strip/VxORL',nmPorts['MonitorsOrl']],
-        'moMetronome':['/strip/Metronome',nmPorts['MonitorsOrl']],
-
+        #MonitorsORL
+        'moDrums':['/strip/Drums',nmPorts['MonitorsORL']],
+        'moBasses':['/strip/Basses',nmPorts['MonitorsORL']],
+        'moGuitars':['/strip/Guitars',nmPorts['MonitorsORL']],
+        'moMxDrums':['/strip/MxDrums',nmPorts['MonitorsORL']],
+        'moMxSynths':['/strip/MxSynths',nmPorts['MonitorsORL']],
+        'moVocals':['/strip/Vocals',nmPorts['MonitorsORL']],
+        'moBassORL':['/strip/BassORL',nmPorts['MonitorsORL']],
+        'moGuitarORL':['/strip/GuitarORL',nmPorts['MonitorsORL']],
+        'moVxORL':['/strip/VxORL',nmPorts['MonitorsORL']],
+        'moMetronome':['/strip/Metronome',nmPorts['MonitorsORL']],
 }
 
 #Parametre gui > NonMixer
@@ -115,9 +146,9 @@ example_path = os_path.dirname(os_path.abspath(__file__)) + '/example/'
 html_path = example_path + 'sebkha.html'
 preset_name = example_path + 'sebkha'
 
-gui = oscwebgui.oscWebGui(port=3333, target='127.0.0.1:3334', appName='oscWebGui', presetName=preset_name,html=html_path)
+gui = oschtmlgui.oscHtmlGui(port=3333, target='127.0.0.1:3335', appName='oscWebGui', presetName=preset_name,html=html_path)
 
-router = oscwebgui.oscRouter(port=3334, pathPatch=pathPatch, paramPatch=paramPatch)
+router = oschtmlgui.oscRouter(port=3334, pathPatch=pathPatch, paramPatch=paramPatch)
 router.start()
 
 gui.main()
