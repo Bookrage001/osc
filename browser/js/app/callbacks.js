Bookrage001/osc

commit b1886a101c9b3a094e649de09e43593a9c2f0e63

widget synchronization and linking is now handled with a single event handler (delegation ftw!)

browser/js/app/widgets.js
@@ -1,6 +1,7 @@
 var strip = require('./widgets/strip'),
     panel = require('./widgets/panel'),
     led = require('./widgets/led'),
+    plot = require('./widgets/plot'),
     fader = require('./widgets/fader'),
     knob = require('./widgets/knob'),
     xy = require('./widgets/xy'),
@@ -16,6 +17,7 @@ module.exports.widgetOptions = {
     strip: strip.options,
     panel: panel.options,
     led: led.options,
+    plot: plot.options,
     fader: fader.options,
     knob: knob.options,
     xy: xy.options,
@@ -32,6 +34,7 @@ module.exports.createWidget = {
     strip: strip.create,
     panel: panel.create,
     led: led.create,
+    plot: plot.create,
     fader: fader.create,
     knob: knob.create,
     xy: xy.create,
@@ -49,48 +52,30 @@ module.exports.categories = {
     'Buttons':['toggle','push','switch'],
     'Pads':['xy','rgb'],
     'Matrix':['multifader','multitoggle','multipush'],
-    'Plots':['led'],
+    'Plots':['plot','led'],
     'Containers':['panel','strip']
 }
 
 module.exports.sync = function() {
-    // Widget that share the same id will update each other
-    // without sending any extra osc message
-    $.each(WIDGETS,function(i,widget) {
-        if (widget.length>1) {
-            var closureSync = function(x) {
-                return function() {
-                    var v = widget[x].getValue()
-                    for (k=0;k<widget.length;k++) {
-                        if (x!=k) {
-                            if (document.body.contains(widget[k][0].parentNode))
-                                widget[k].setValue(v,false,false)
-                        }
-                    }
+    $('body').off('sync.global').on('sync.global',function(e,id,widget){
+        // Widget that share the same id will update each other
+        // without sending any extra osc message
+        if (WIDGETS[id] && WIDGETS[id].length>1) {
+            var v = widget.getValue()
+            for (i in WIDGETS[id]) {
+                if (WIDGETS[id]!=widget) {
+                    WIDGETS[id][i].setValue(v,false,false)
                 }
-            }
-            for (j in widget) {
-                widget[j].off('sync.id').on('sync.id',closureSync(j))
             }
         }
-    })
-    // widgets that share the same linkId will update each other.
-    // Updated widgets will send osc messages normally
-    $.each(WIDGETS_LINKED,function(i,widget) {
-        if (widget.length>1) {
-            var closureSync = function(x) {
-                return function() {
-                    var v = widget[x].getValue()
-                    for (k=0;k<widget.length;k++) {
-                        if (x!=k) {
-                            if (document.body.contains(widget[k][0].parentNode))
-                                widget[k].setValue(v,true,false)
-                        }
-                    }
+        // widgets that share the same linkId will update each other.
+        // Updated widgets will send osc messages normally
+        if (WIDGETS_LINKED[id] && WIDGETS_LINKED[id].length>1) {
+            var v = widget.getValue()
+            for (i in WIDGETS_LINKED[id]) {
+                if (WIDGETS_LINKED[id]!=widget) {
+                    WIDGETS_LINKED[id][i].setValue(v,true,false)
                 }
-            }
-            for (j in widget) {
-                widget[j].off('sync.link').on('sync.link',closureSync(j))
             }
         }
     })

browser/js/app/widgets/fader.js
@@ -113,7 +113,7 @@ module.exports.create = function(widgetData,container) {
         widget.sendValue(v)
         widget.showValue(v)
 
-        widget.trigger('sync')
+        widget.trigger('sync',[widgetData.id,widget])
     })
 
     var off = 0
@@ -131,7 +131,7 @@ module.exports.create = function(widgetData,container) {
             widget.sendValue(v)
             widget.showValue(v)
 
-            widget.trigger('sync')
+            widget.trigger('sync',[widgetData.id,widget])
 
         }
 
@@ -156,7 +156,7 @@ module.exports.create = function(widgetData,container) {
         widget.sendValue(v)
         widget.showValue(v)
 
-        widget.trigger('sync')
+        widget.trigger('sync',[widgetData.id,widget])
 
     })
 
@@ -192,7 +192,7 @@ module.exports.create = function(widgetData,container) {
         widget.showValue(v)
 
 
-        if (sync) widget.trigger('sync')
+        if (sync) widget.trigger('sync',[widgetData.id,widget])
         if (send) widget.sendValue(v)
     }
 

browser/js/app/widgets/knob.js
@@ -88,7 +88,7 @@ module.exports.create = function(widgetData,container) {
         widget.sendValue(v)
         widget.showValue(v)
 
-        widget.trigger('sync')
+        widget.trigger('sync',[widgetData.id,widget])
     })
 
 
@@ -119,7 +119,7 @@ module.exports.create = function(widgetData,container) {
             var v = widget.getValue(r)
 
             widget.sendValue(v)
-            widget.trigger('sync')
+            widget.trigger('sync',[widgetData.id,widget])
             widget.showValue(v)
 
         }
@@ -152,7 +152,7 @@ module.exports.create = function(widgetData,container) {
         var v = widget.getValue(r)
 
         widget.sendValue(v)
-        widget.trigger('sync')
+        widget.trigger('sync',[widgetData.id,widget])
         widget.showValue(v)
 
     })
@@ -203,7 +203,7 @@ module.exports.create = function(widgetData,container) {
 
         widget.showValue(v)
 
-        if (sync) widget.trigger('sync')
+        if (sync) widget.trigger('sync',[widgetData.id,widget])
         if (send) widget.sendValue(v)
     }
     input.change(function(){

browser/js/app/widgets/push.js
@@ -66,7 +66,7 @@ module.exports.create = function(widgetData,container) {
             if (send) widget.sendValue(v)
         }
 
-        if (sync) widget.trigger('sync')
+        if (sync) widget.trigger('sync',[widgetData.id,widget])
 
     }
     widget.sendValue = function(v) {

browser/js/app/widgets/rgb.js
@@ -92,7 +92,7 @@ module.exports.create = function(widgetData,container) {
                 var v = widget.getValue()
                 widget.sendValue(v)
                 widget.showValue(v)
-                widget.trigger('sync')
+                widget.trigger('sync',[widgetData.id,widget])
 
             }
 
@@ -115,7 +115,7 @@ module.exports.create = function(widgetData,container) {
             widget.sendValue(v)
             widget.showValue(v)
 
-            widget.trigger('sync')
+            widget.trigger('sync',[widgetData.id,widget])
 
         })
 
@@ -138,7 +138,7 @@ module.exports.create = function(widgetData,container) {
 
                 widget.sendValue(v)
                 widget.showValue(v)
-                widget.trigger('sync')
+                widget.trigger('sync',[widgetData.id,widget])
 
             }
 
@@ -164,7 +164,7 @@ module.exports.create = function(widgetData,container) {
             widget.sendValue(v)
             widget.showValue(v)
 
-            widget.trigger('sync')
+            widget.trigger('sync',[widgetData.id,widget])
 
         })
 
@@ -208,7 +208,7 @@ module.exports.create = function(widgetData,container) {
 
 
 
-            if (sync) widget.trigger('sync')
+            if (sync) widget.trigger('sync',[widgetData.id,widget])
             if (send) widget.sendValue(v)
         }
         widget.sendValue = function(v) {

browser/js/app/widgets/switch.js
@@ -65,7 +65,7 @@ module.exports.create = function(widgetData,container) {
         			next()
         		})
                 if (send) widget.sendValue(widget.value)
-                if (sync) widget.trigger('sync')
+                if (sync) widget.trigger('sync',[widgetData.id,widget])
             }
 
         }

browser/js/app/widgets/toggle.js
@@ -67,7 +67,7 @@ module.exports.create = function(widgetData,container) {
             if (send) widget.sendValue(widgetData.off)
         }
 
-        if (sync) widget.trigger('sync')
+        if (sync) widget.trigger('sync',[widgetData.id,widget])
 
     }
     widget.sendValue = function(v) {

browser/js/app/widgets/xy.js
@@ -86,7 +86,7 @@ module.exports.create = function(widgetData,container) {
             var v = widget.getValue()
             widget.sendValue(v)
             widget.showValue(v)
-            widget.trigger('sync')
+            widget.trigger('sync',[widgetData.id,widget])
 
         }
 
@@ -109,7 +109,7 @@ module.exports.create = function(widgetData,container) {
         var v = widget.getValue()
         widget.sendValue(v)
         widget.showValue(v)
-        widget.trigger('sync')
+        widget.trigger('sync',[widgetData.id,widget])
 
     })
 
@@ -141,7 +141,7 @@ module.exports.create = function(widgetData,container) {
         handle.width = w
 
         widget.showValue(v)
-        if (sync) widget.trigger('sync')
+        if (sync) widget.trigger('sync',[widgetData.id,widget])
         if (send) widget.sendValue(v)
     }
     widget.sendValue = function(v) {

commit 0736cc4d2969a18ccd1a751743fc70bd1cd2f161

widget link fixed (broken since b1886a101c9b3a094e649de09e43593a9c2f0e63): - the sync event now carries the linkId, required for proper link sync - incomming osc messages always trigger the sync event, thus ensuring that plots and links are well synchronized

browser/js/app/callbacks.js
@@ -15,7 +15,7 @@ module.exports = {
         // update
         if (WIDGETS[id]!=undefined) {
             for (i in WIDGETS[id]){
-                 WIDGETS[id][i].setValue(data.args,false,false)
+                 WIDGETS[id][i].setValue(data.args,false,true)
             }
         }
 

browser/js/app/widgets.js
@@ -60,7 +60,7 @@ module.exports.categories = {
 }
 
 module.exports.sync = function() {
-    $('body').off('sync.global').on('sync.global',function(e,id,widget){
+    $('body').off('sync.global').on('sync.global',function(e,id,widget,linkId){
         // Widget that share the same id will update each other
         // without sending any extra osc message
         if (WIDGETS[id] && WIDGETS[id].length>1) {
@@ -73,11 +73,11 @@ module.exports.sync = function() {
         }
         // widgets that share the same linkId will update each other.
         // Updated widgets will send osc messages normally
-        if (WIDGETS_LINKED[id] && WIDGETS_LINKED[id].length>1) {
+        if (WIDGETS_LINKED[linkId] && WIDGETS_LINKED[linkId].length>1) {
             var v = widget.getValue()
-            for (i in WIDGETS_LINKED[id]) {
-                if (WIDGETS_LINKED[id][i]!=widget) {
-                    WIDGETS_LINKED[id][i].setValue(v,true,false)
+            for (i in WIDGETS_LINKED[linkId]) {
+                if (WIDGETS_LINKED[linkId][i]!=widget) {
+                    WIDGETS_LINKED[linkId][i].setValue(v,true,false)
                 }
             }
         }

browser/js/app/widgets/fader.js
@@ -113,7 +113,7 @@ module.exports.create = function(widgetData,container) {
         widget.sendValue(v)
         widget.showValue(v)
 
-        widget.trigger('sync',[widgetData.id,widget])
+        widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
     })
 
     var off = 0
@@ -131,7 +131,7 @@ module.exports.create = function(widgetData,container) {
             widget.sendValue(v)
             widget.showValue(v)
 
-            widget.trigger('sync',[widgetData.id,widget])
+            widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
         }
 
@@ -156,7 +156,7 @@ module.exports.create = function(widgetData,container) {
         widget.sendValue(v)
         widget.showValue(v)
 
-        widget.trigger('sync',[widgetData.id,widget])
+        widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
     })
 
@@ -192,7 +192,7 @@ module.exports.create = function(widgetData,container) {
         widget.showValue(v)
 
 
-        if (sync) widget.trigger('sync',[widgetData.id,widget])
+        if (sync) widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
         if (send) widget.sendValue(v)
     }
 

browser/js/app/widgets/knob.js
@@ -88,7 +88,7 @@ module.exports.create = function(widgetData,container) {
         widget.sendValue(v)
         widget.showValue(v)
 
-        widget.trigger('sync',[widgetData.id,widget])
+        widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
     })
 
 
@@ -119,7 +119,7 @@ module.exports.create = function(widgetData,container) {
             var v = widget.getValue(r)
 
             widget.sendValue(v)
-            widget.trigger('sync',[widgetData.id,widget])
+            widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
             widget.showValue(v)
 
         }
@@ -152,7 +152,7 @@ module.exports.create = function(widgetData,container) {
         var v = widget.getValue(r)
 
         widget.sendValue(v)
-        widget.trigger('sync',[widgetData.id,widget])
+        widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
         widget.showValue(v)
 
     })
@@ -203,7 +203,7 @@ module.exports.create = function(widgetData,container) {
 
         widget.showValue(v)
 
-        if (sync) widget.trigger('sync',[widgetData.id,widget])
+        if (sync) widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
         if (send) widget.sendValue(v)
     }
     input.change(function(){

browser/js/app/widgets/push.js
@@ -66,7 +66,7 @@ module.exports.create = function(widgetData,container) {
             if (send) widget.sendValue(v)
         }
 
-        if (sync) widget.trigger('sync',[widgetData.id,widget])
+        if (sync) widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
     }
     widget.sendValue = function(v) {

browser/js/app/widgets/rgb.js
@@ -92,7 +92,7 @@ module.exports.create = function(widgetData,container) {
                 var v = widget.getValue()
                 widget.sendValue(v)
                 widget.showValue(v)
-                widget.trigger('sync',[widgetData.id,widget])
+                widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
             }
 
@@ -115,7 +115,7 @@ module.exports.create = function(widgetData,container) {
             widget.sendValue(v)
             widget.showValue(v)
 
-            widget.trigger('sync',[widgetData.id,widget])
+            widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
         })
 
@@ -138,7 +138,7 @@ module.exports.create = function(widgetData,container) {
 
                 widget.sendValue(v)
                 widget.showValue(v)
-                widget.trigger('sync',[widgetData.id,widget])
+                widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
             }
 
@@ -164,7 +164,7 @@ module.exports.create = function(widgetData,container) {
             widget.sendValue(v)
             widget.showValue(v)
 
-            widget.trigger('sync',[widgetData.id,widget])
+            widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
         })
 
@@ -208,7 +208,7 @@ module.exports.create = function(widgetData,container) {
 
 
 
-            if (sync) widget.trigger('sync',[widgetData.id,widget])
+            if (sync) widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
             if (send) widget.sendValue(v)
         }
         widget.sendValue = function(v) {

browser/js/app/widgets/switch.js
@@ -65,7 +65,7 @@ module.exports.create = function(widgetData,container) {
         			next()
         		})
                 if (send) widget.sendValue(widget.value)
-                if (sync) widget.trigger('sync',[widgetData.id,widget])
+                if (sync) widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
             }
 
         }

browser/js/app/widgets/toggle.js
@@ -67,7 +67,7 @@ module.exports.create = function(widgetData,container) {
             if (send) widget.sendValue(widgetData.off)
         }
 
-        if (sync) widget.trigger('sync',[widgetData.id,widget])
+        if (sync) widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
     }
     widget.sendValue = function(v) {

browser/js/app/widgets/xy.js
@@ -86,7 +86,7 @@ module.exports.create = function(widgetData,container) {
             var v = widget.getValue()
             widget.sendValue(v)
             widget.showValue(v)
-            widget.trigger('sync',[widgetData.id,widget])
+            widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
         }
 
@@ -109,7 +109,7 @@ module.exports.create = function(widgetData,container) {
         var v = widget.getValue()
         widget.sendValue(v)
         widget.showValue(v)
-        widget.trigger('sync',[widgetData.id,widget])
+        widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
 
     })
 
@@ -141,7 +141,7 @@ module.exports.create = function(widgetData,container) {
         handle.width = w
 
         widget.showValue(v)
-        if (sync) widget.trigger('sync',[widgetData.id,widget])
+        if (sync) widget.trigger('sync',[widgetData.id,widget,widgetData.linkId])
         if (send) widget.sendValue(v)
     }
     widget.sendValue = function(v) {