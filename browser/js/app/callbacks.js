3b5fabcc8ba8bfedac08f206eade02ede6516ba6
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2016-04-01 00:16:12 +0200

    let's make the lobby suck a little bit less

--- browser/js/app/callbacks.js
+++ browser/js/app/callbacks.js
@@ -42,19 +42,20 @@ module.exports = {
 
     sessionList: function(event,data){
         var data = data || event
-        $('#lobby').append('\
-            <div class="main">\
-                <div class="header">\
-                    Session selection\
-                </div>\
-                <div class="list"></div>\
-            </div>')
+        $('#lobby').append(`
+            <div class="main">
+                <div class="header">
+                    Open Stage Control
+                </div>
+                <div class="list"></div>
+                <div class="footer"></div>
+            </div>`)
 
         for (i in data) {
-            $('#lobby .list').append('<li><a class="btn load" data-session="'+data[i]+'">'+data[i]+'<span>'+icon('remove')+'</span></a></li>')
+            $('#lobby .list').append('<a class="btn load" data-session="'+data[i]+'">'+data[i]+'<span>'+icon('remove')+'</span></a>')
         }
-        $('#lobby .list').append('<a class="btn browse">...</a>')
-        $('#lobby .list').append('<a class="btn new">New</a>')
+        $('#lobby .footer').append('<a class="btn browse">'+icon('folder-open')+' Browse</a>')
+        $('#lobby .footer').append('<a class="btn new">'+icon('file-o')+' New</a>')
         $('#lobby .load').click(function(e){
             e.stopPropagation()
             IPC.send('sessionOpen',{path:$(this).data('session')})
@@ -62,7 +63,7 @@ module.exports = {
         $('#lobby a span').click(function(e){
             e.stopPropagation()
             IPC.send('sessionRemoveFromHistory',$(this).parent().data('session'))
-            $(this).parents('li').hide()
+            $(this).parents('a').remove()
         })
         $('#lobby .browse').click(function(e){
             e.stopPropagation()
