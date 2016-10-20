f525df42724de67f7b847d98f411ac1ac66a2a35
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2016-10-20 19:13:49 +0200

    widget: add path splitting support in base class (triggered by this.split property)

--- src/browser/js/app/widgets/common/_widgets_base.js
+++ src/browser/js/app/widgets/common/_widgets_base.js
@@ -13,7 +13,23 @@ _widget_base.prototype.sendValue = function() {
         target:this.widgetData.target,
         path:this.widgetData.path,
         precision:this.widgetData.precision,
-        args:args
+        args:args,
+        syncOnly:this.split?true:false
     })
+
+    if (this.split) {
+        var n = 0
+        for (i in this.split) {
+            sendOsc({
+                target:this.widgetData.target,
+                path:this.split[i],
+                precision:this.widgetData.precision,
+                args:this.widgetData.preArgs.concat(this.value[n]),
+                sync:false
+
+            })
+            n++
+        }
+    }
 
 }
