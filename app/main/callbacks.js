057836082ca445e71d9f2de07756860d48d9ef21
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2016-10-21 17:27:38 +0200

    main: remove sync=false preventing osc sends to sync hosts (since actual clients sync is handled before that anyway)

--- app/main/callbacks.js
+++ app/main/callbacks.js
@@ -71,7 +71,7 @@ module.exports =  {
 
 	        var targets = []
 
-	        if (settings.read('syncTargets') && data.sync!==false) Array.prototype.push.apply(targets, settings.read('syncTargets'))
+	        if (settings.read('syncTargets')) Array.prototype.push.apply(targets, settings.read('syncTargets'))
 	        if (data.target) Array.prototype.push.apply(targets, data.target)
 
 
