ddab3e416d036eee0900a563b884a395f75d26cb
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2016-11-03 14:50:43 +0100

    docs: use latest tag name instead of package.json's version

--- resources/docs/js/extra.js
+++ resources/docs/js/extra.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $.getJSON('https://api.github.com/repos/jean-emmanuel/open-stage-control',function(data){
         $('#github-stars').text(data.stargazers_count)
     })
-    $.getJSON('https://raw.githubusercontent.com/jean-emmanuel/open-stage-control/master/app/package.json',function(data){
-        $('#github-version').text('v'+data.version)
+    $.getJSON('https://api.github.com/repos/jean-emmanuel/open-stage-control/tags',function(data){
+        $('#github-version').text(data[0].name)
     })
 })
