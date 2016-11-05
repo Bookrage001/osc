bddf5f8fa701d2ac7cb55aa98cfc0c818efc5f73
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2016-11-05 12:10:30 +0100

    lobby: make it smooth too

--- src/browser/js/app/callbacks.js
+++ src/browser/js/app/callbacks.js
@@ -60,14 +60,16 @@ module.exports = {
 
     sessionList: function(event,data){
         var data = data || event
-        $('#lobby').append(`
+        var lobby = $(`
             <div class="main">
                 <div class="header">
                     Open Stage Control
                 </div>
                 <div class="list"></div>
                 <div class="footer"></div>
-            </div>`)
+            </div>`),
+            list = lobby.find('.list'),
+            footer = lobby.find('.footer')
 
         for (i in data) {
             var max = 50
@@ -79,26 +81,30 @@ module.exports = {
                 path = path.substr(0,Math.floor((path.length)/2)-(length-max)/2) + '...' + path.substr(Math.ceil((path.length)/2)+(length-max)/2, path.length)
             }
 
-            $('#lobby .list').append('<a class="btn load" data-session="'+data[i]+'">'+file+' <em style="opacity:0.45">('+path+')</em><span>'+icon('remove')+'</span></a>')
+            list.append('<a class="btn load" data-session="'+data[i]+'">'+file+' <em style="opacity:0.45">('+path+')</em><span>'+icon('remove')+'</span></a>')
         }
-        $('#lobby .footer').append('<a class="btn browse">'+icon('folder-open')+' Browse</a>')
-        $('#lobby .footer').append('<a class="btn new">'+icon('file-o')+' New</a>')
-        $('#lobby .load').click(function(e){
+        footer.append('<a class="btn browse">'+icon('folder-open')+' Browse</a>')
+        footer.append('<a class="btn new">'+icon('file-o')+' New</a>')
+        lobby.find('.load').click(function(e){
             e.stopPropagation()
             IPC.send('sessionOpen',{path:$(this).data('session')})
         })
-        $('#lobby a span').click(function(e){
+        lobby.find('a span').click(function(e){
             e.stopPropagation()
             IPC.send('sessionRemoveFromHistory',$(this).parent().data('session'))
             $(this).parents('a').remove()
         })
-        $('#lobby .browse').click(function(e){
+        lobby.find('.browse').click(function(e){
             e.stopPropagation()
             actions.sessionBrowse()
         })
-        $('#lobby .new').click(function(e){
+        lobby.find('.new').click(function(e){
             e.stopPropagation()
             init([{}],function(){$('#open-toggle, .enable-editor').click();$('.editor-root').trigger('mousedown.editor')})
+        })
+        $('#lobby').append(lobby)
+        setTimeout(()=>{
+            lobby.addClass('loaded')
         })
     },
 
