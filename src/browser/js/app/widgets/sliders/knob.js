70b3e7156851f682da25e222ab6a84f11251ea74
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2016-11-22 18:51:29 +0100

    knob: swaggify compact gauge (link alpha & distance to origin)

--- src/browser/js/app/widgets/sliders/knob.js
+++ src/browser/js/app/widgets/sliders/knob.js
@@ -162,7 +162,7 @@ Knob.prototype.draw = function(){
         this.ctx.arc(this.width / 2, this.height / 2, this.minDimension / 2 - (this.gaugeWidth + this.margin) * PXSCALE, min, max)
         this.ctx.stroke()
 
-        this.ctx.globalAlpha = 0.3
+        this.ctx.globalAlpha = 0.2 + 0.2 * Math.abs(d-o) / (d<o?o-min:max-o)
         this.ctx.beginPath()
         this.ctx.strokeStyle = this.colors.gauge
         this.ctx.arc(this.width / 2, this.height / 2, this.minDimension / 2 - (this.gaugeWidth + this.margin) * PXSCALE, Math.min(o,d), Math.max(o,d))
@@ -173,6 +173,7 @@ Knob.prototype.draw = function(){
         this.ctx.strokeStyle = this.colors.track
         this.ctx.arc(this.width / 2, this.height / 2, this.minDimension / 2 - (this.gaugeWidth/2 + this.margin) * PXSCALE, min, max)
         this.ctx.stroke()
+
 
         this.ctx.globalAlpha = 1
 
