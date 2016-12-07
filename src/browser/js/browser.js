f5e5360021d2a2b9730b33ac9463d10a985fdcf9
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2016-12-07 20:49:20 +0100

    browser: remove useless app.js

--- src/browser/js/browser.js
+++ src/browser/js/browser.js
@@ -1,4 +1,47 @@
-// mainProcess & renderProcess communication engine
-// electron's ipc module is not available here, we use socket.io as a replacement
+///////////////////////
 
-require('./app')
+SESSION = []
+STATE = []
+
+CLIPBOARD = null
+EDITING = false
+
+TRAVERSING = false
+
+OSCSYNCONLY = false
+
+TABS = {}
+
+
+MISC = {
+    iterators: {
+        widget:{},
+        tab:{}
+    }
+}
+
+
+PXSCALE =  getComputedStyle(document.documentElement).getPropertyValue("--pixel-scale")
+INITIALZOOM = PXSCALE
+
+
+///////////////////////
+
+$ = jQuery = require('./jquery/jquery.min')
+require('./jquery/jquery.ui')
+require('./jquery/jquery.drag')
+require('./jquery/jquery.resize')
+require('./jquery/jquery.fake-input')
+
+///////////////////////
+
+var callbacks = require('./app/callbacks'),
+    ipc = require('./app/ipc')
+
+
+
+///////////////////////
+$(document).ready(function(){
+    LOADING = require('./app/utils').loading('Connecting server...')
+    ipc.send('ready')
+})
