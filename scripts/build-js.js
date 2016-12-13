473f059d66e2ec3b38f00d6508c53a4f782acbbc
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2016-12-13 15:14:12 +0100

    dev: generate js source map file to help debugging

--- scripts/build-js.js
+++ scripts/build-js.js
@@ -1,12 +1,15 @@
 var browserify = require('browserify'),
+    exorcist = require('exorcist'),
     path = require('path')
 
 
-var b = browserify(path.resolve(__dirname + '/../src/browser/js/browser.js'));
+var b = browserify(path.resolve(__dirname + '/../src/browser/js/browser.js'),{debug:true});
 
 // bypass syntax-error check if node version is < 6
 if (parseInt(process.version.split('.')[0].replace('v',''))<6) {
     b.pipeline.get('syntax').splice(0, 1);
 }
 
-b.bundle().pipe(process.stdout);
+b.bundle()
+ .pipe(exorcist(path.resolve(__dirname + '/../app/browser/scripts.js.map')))
+ .pipe(process.stdout)
