4a606b810923b7f62c9b5f5988b43f1473fd1e2f
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2016-12-13 22:28:53 +0100

    debug: load the source map and print errors in the main process' stdout (add a source map file parser)

--- src/browser/js/browser.js
+++ src/browser/js/browser.js
@@ -13,6 +13,27 @@ TABS = {}
 PXSCALE =  getComputedStyle(document.documentElement).getPropertyValue("--pixel-scale")
 INITIALZOOM = PXSCALE
 
+///////////////////////
+
+sourceMap = require('./libs/source-map.min.js')
+
+var request = new XMLHttpRequest()
+
+request.open('GET', 'scripts.js.map', true)
+
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+    var data = JSON.parse(request.responseText),
+        smc = new sourceMap.SourceMapConsumer(data)
+
+    window.onerror = function(error,url,row,col) {
+        var data = smc.originalPositionFor({line:row,column:col})
+        ipc.send('log', `[Renderer process error]\n${error}\nSource: ${data.source} @ line ${data.line}`)
+    }
+  }
+}
+
+request.send()
 
 ///////////////////////
 
