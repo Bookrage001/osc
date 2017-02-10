46ddfbe5918130b258bcdaaf4144b78bef3dc10c
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-02-11 00:58:54 +0100

    remote-exec: fix crash when editing widgets in off tabs + make /EXEC edit strict by default (replace attributes) + add  /EXEC edit_soft that does an object merging

--- src/browser/js/app/remote-exec.js
+++ src/browser/js/app/remote-exec.js
@@ -10,7 +10,23 @@ var callbacks = {
         if (!containers.length) return
         for (var i=containers.length-1;i>=0;i--) {
             var container = containers[i].widget.parent(),
-                data = getObjectData(container)
+                data = containers[i].widgetData
+
+            for (var k in newdata) {
+                data[k] = newdata[k]
+            }
+            updateDom(container,data,true)
+        }
+    },
+    edit_soft: function(args) {
+        var [id, json] = args,
+            newdata = JSON.parse(json),
+            containers = widgetManager.getWidgetById(id)
+
+        if (!containers.length) return
+        for (var i=containers.length-1;i>=0;i--) {
+            var container = containers[i].widget.parent(),
+                data = containers[i].widgetData
 
             $.extend(true,data,newdata)
             updateDom(container,data,true)
