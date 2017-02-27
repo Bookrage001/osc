8ca2e5a87e13c48e5cd24d50a1bc27f49fc6efd9
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-02-27 22:24:01 +0100

    launcher: adjust height (+midi line)

--- app/main/main.js
+++ app/main/main.js
@@ -60,7 +60,7 @@ if (cli) {
         launcher
 
     app.on('ready',function(){
-        launcher = require('./electron-window')({address:address, shortcuts:false, width:600, height:482})
+        launcher = require('./electron-window')({address:address, shortcuts:false, width:600, height:510})
     })
 
     ipcMain.on('start',function(e, options){
