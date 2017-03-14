8bd7c6ba3714bd86835b14c0df274d21e755f18b
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-03-14 16:36:26 +0100

    panel: start implementing Visual Format Language layout option

--- src/browser/js/app/widgets/containers/panel.js
+++ src/browser/js/app/widgets/containers/panel.js
@@ -1,4 +1,5 @@
-var _widgets_base = require('../common/_widgets_base')
+var _widgets_base = require('../common/_widgets_base'),
+    autolayout = require('autolayout')
 
 module.exports = class Panel extends _widgets_base {
 
@@ -18,6 +19,11 @@ module.exports = class Panel extends _widgets_base {
             scroll:true,
             color:'auto',
             css:'',
+
+            _layout:'layout',
+
+            layout:'',
+            spacing:0,
 
             _children:'chilren',
 
@@ -50,6 +56,36 @@ module.exports = class Panel extends _widgets_base {
             parsewidgets(widgetData.widgets,this.widget)
         }
 
+        if (widgetData.layout != '') {
+            try {
+
+                this.layout = new autolayout.View({
+                    constraints: autolayout.VisualFormat.parse(widgetData.layout.split('\\n').join('\n').split(' ').join(''), {extended: true}),
+                    spacing: widgetData.spacing
+                });
+
+                this.widget.on('resize',(e, w, h)=>{
+                    if (!w) return
+                    this.layout.setSize(w,h)
+                    this.applyLayout()
+                })
+
+            } catch(err) {
+                console.log('Layout parsing error: ' + err.message)
+            }
+
+        }
+    }
+    applyLayout(){
+        this.widget.find('> .widget').each((i,widget)=>{
+            var id = widget.abstract.widgetData.id
+            if (!this.layout.subViews[id]) return
+            var $widget = $(widget).addClass('absolute-position')
+            for (var prop of ['height', 'width', 'top', 'left']) {
+                delete widget.abstract.widgetData[prop]
+                $widget.css(prop, this.layout.subViews[id][prop] + 'px')
+            }
+        })
     }
 
 }
