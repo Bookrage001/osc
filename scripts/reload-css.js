dd72b9bb6d92279031e28611a14be1ff063500de
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-06-13 19:42:47 +0200

    dev: update scss hot reload to work with new ipc parser

--- scripts/reload-css.js
+++ scripts/reload-css.js
@@ -1,4 +1,6 @@
-var socket = require('socket.io-client')
-var ipc = socket.connect('http://127.0.0.1:8080')
+var socket = require('socket.io-client'),
+    customParser= require('socket.io-msgpack-parser')
+
+var ipc = socket.connect('http://127.0.0.1:8080', {parser:customParser})
 ipc.emit('reloadCss')
 setTimeout(process.exit,500)
