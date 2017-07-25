6b13d749714d819102e3a80ccb68a3c82c77a822
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-07-25 12:25:03 +0200

    input: border fix when in strip/panel

--- app/main/electron-app.js
+++ app/main/electron-app.js
@@ -1,5 +1,7 @@
-var {app, dialog} = require('electron'),
-    settings = require('./settings')
+var {app, dialog, Tray, Menu} = require('electron'),
+    settings = require('./settings'),
+    path = require('path'),
+    tray
 
 dialog.showErrorBox = function(title,err) {
     console.log(title + ': ' + err)
@@ -17,6 +19,21 @@ app.on('window-all-closed', function() {
     if (process.platform != 'darwin') {
         app.quit()
     }
+})
+
+app.on('ready', () => {
+  tray = new Tray(path.resolve(__dirname + '/../../resources/images/logo.png'))
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'New client window', type: 'normal'},
+    {label: 'Item2', type: 'radio'}
+  ])
+
+  // Make a change to the context menu
+  contextMenu.items[1].checked = false
+
+  tray.setToolTip(settings.read('appName'))
+  // Call this again for Linux because we modified the context menu
+  tray.setContextMenu(contextMenu)
 })
 
 module.exports = app
