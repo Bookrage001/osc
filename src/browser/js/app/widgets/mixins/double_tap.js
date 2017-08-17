e67a6c3a1ec769e24ac2cab9c76788b920f0bc7e
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-08-17 23:58:32 +0200

    doubletap: avoid triple tap to act as 2 doubles

--- src/browser/js/app/widgets/mixins/double_tap.js
+++ src/browser/js/app/widgets/mixins/double_tap.js
@@ -11,17 +11,24 @@ module.exports = (element, callback)=>{
         var tapLength = tapTime - lastTapTime
 
     	if (
+            
             tapLength < DOUBLE_TAP_TIME && touchId == d.identifier &&
             Math.abs(lastTapX - d.pageX) < 20 &&
             Math.abs(lastTapY - d.pageY) < 20
-        ) {
-            callback()
-        }
 
-        lastTapTime = tapTime
-        touchId = d.identifier
-        lastTapX = d.pageX
-        lastTapY = d.pageY
+        ) {
+
+            callback()
+            lastTapTime = 0
+
+        } else {
+
+            lastTapTime = tapTime
+            touchId = d.identifier
+            lastTapX = d.pageX
+            lastTapY = d.pageY
+
+        }
 
     })
 
