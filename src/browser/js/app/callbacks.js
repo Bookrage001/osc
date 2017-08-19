Bookrage001/osc

commit d807032ace34fadbd9f6ec35286a96324ba7ce0f

lobby: show focus hint only with keyboard navigation (tab)

src/browser/js/app/callbacks.js
@@ -61,6 +61,9 @@ var callbacks = module.exports = {
             e.preventDefault()
             e.stopPropagation()
             ipc.send('sessionOpen',{path:$(this).data('session')})
+        }).on('mousedown touchstart', function(e){
+            e.preventDefault()
+            $(this).blur()
         })
 
         if (!READ_ONLY) {
@@ -71,16 +74,22 @@ var callbacks = module.exports = {
                 $(this).parents('a').remove()
             })
 
-            footer.append('<a class="btn browse">'+icon('folder-open')+' Browse</a>')
-            footer.append('<a class="btn new">'+icon('file-o')+' New</a>')
+            footer.append('<a href="#" tabindex="0" class="btn browse">'+icon('folder-open')+' Browse</a>')
+            footer.append('<a href="#" tabindex="0" class="btn new">'+icon('file-o')+' New</a>')
 
             lobby.find('.browse').click(function(e){
                 e.stopPropagation()
                 actions.sessionBrowse()
+            }).on('mousedown touchstart', function(e){
+                e.preventDefault()
+                $(this).blur()
             })
             lobby.find('.new').click(function(e){
                 e.stopPropagation()
                 module.exports.sessionNew()
+            }).on('mousedown touchstart', function(e){
+                e.preventDefault()
+                $(this).blur()
             })
         }
 

src/browser/scss/style.scss
@@ -166,7 +166,19 @@ body {
         flex-grow: 0;
         border-top:2rem solid var(--color-bg);
         a  {
-            background:var(--color-raised)
+            background:var(--color-raised);
+            &:focus {
+                &:after {
+                    content:"";
+                    position: absolute;
+                    top:0;
+                    left:0;
+                    bottom:0;
+                    right:0;
+                    border:1rem solid var(--color-accent);
+                    opacity: 0.3;
+                }
+            }
         }
     }
     a {

commit 0795c4cd169e206b459ae6df9d4e6c893b7f69ce

tablets: fix regression introduced in d807032 breaking loby for touch devices (issue #84 & issue #78)

src/browser/js/app/callbacks.js
@@ -62,7 +62,7 @@ var callbacks = module.exports = {
             e.stopPropagation()
             ipc.send('sessionOpen',{path:$(this).data('session')})
         }).on('mousedown touchstart', function(e){
-            e.preventDefault()
+            if (e.type == 'mousedown') e.preventDefault()
             $(this).blur()
         })
 
@@ -81,14 +81,14 @@ var callbacks = module.exports = {
                 e.stopPropagation()
                 actions.sessionBrowse()
             }).on('mousedown touchstart', function(e){
-                e.preventDefault()
+                if (e.type == 'mousedown') e.preventDefault()
                 $(this).blur()
             })
             lobby.find('.new').click(function(e){
                 e.stopPropagation()
                 module.exports.sessionNew()
             }).on('mousedown touchstart', function(e){
-                e.preventDefault()
+                if (e.type == 'mousedown') e.preventDefault()
                 $(this).blur()
             })
         }