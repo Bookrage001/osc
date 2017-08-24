e2e2e6d549d9dcc290b362c42cb07c857f0ab519
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-08-24 23:30:17 +0200

    build: add --fast option to build-js

--- scripts/build-js.js
+++ scripts/build-js.js
@@ -2,16 +2,20 @@ var browserify = require('browserify'),
     exorcist = require('exorcist'),
     path = require('path'),
     babelify = require('babelify'),
-    prod = process.argv.indexOf('--prod') != -1
+    prod = process.argv.indexOf('--prod') != -1,
+    fast = process.argv.indexOf('--fast') != -1
+
 
 if (prod) console.warn('\x1b[36m%s\x1b[0m', 'Building minified js bundle for production... This may take a while... ');
 
-var b = browserify(path.resolve(__dirname + '/../src/browser/js/browser.js'), {debug:true});
+var b = browserify(path.resolve(__dirname + '/../src/browser/js/browser.js'), {debug:!fast});
 
 b.transform(babelify, {presets: ["es2015"], plugins: ["transform-object-rest-spread"]})
 
 if (prod) b.transform('uglifyify', {global: true})
 
-b.bundle()
- .pipe(exorcist(path.resolve(__dirname + '/../app/browser/scripts.js.map')))
- .pipe(process.stdout)
+b = b.bundle()
+
+if (!fast) b.pipe(exorcist(path.resolve(__dirname + '/../app/browser/scripts.js.map')))
+
+b.pipe(process.stdout)
