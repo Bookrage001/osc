d57ef1a6e26a3a599befa1c1448aac943b3b04ed
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2017-08-25 15:26:15 +0200

    package: fix js build size (strip out src-map)

--- scripts/build-js.js
+++ scripts/build-js.js
@@ -4,18 +4,19 @@ var browserify = require('browserify'),
     babelify = require('babelify'),
     prod = process.argv.indexOf('--prod') != -1,
     fast = process.argv.indexOf('--fast') != -1
+    b
 
 
 if (prod) console.warn('\x1b[36m%s\x1b[0m', 'Building minified js bundle for production... This may take a while... ');
 
-var b = browserify(path.resolve(__dirname + '/../src/browser/js/browser.js'), {debug:!fast});
+b = browserify(path.resolve(__dirname + '/../src/browser/js/browser.js'), {debug:!fast})
 
-b.transform(babelify, {presets: ["es2015"], plugins: ["transform-object-rest-spread"]})
+b = b.transform(babelify, {presets: ["es2015"], plugins: ["transform-object-rest-spread"]})
 
-if (prod) b.transform('uglifyify', {global: true})
+if (prod) b = b.transform('uglifyify', {global: true})
 
 b = b.bundle()
 
-if (!fast) b.pipe(exorcist(path.resolve(__dirname + '/../app/browser/scripts.js.map')))
+if (!fast) b = b.pipe(exorcist(path.resolve(__dirname + '/../app/browser/scripts.js.map')))
 
 b.pipe(process.stdout)
