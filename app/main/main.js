d0aa28b1eddbbe80023179a26910e7454e79d30f
bordun <jean-emmanuel.doucet@groolot.net>
2017-09-16 15:59:25 +0200

    launcher; re-disable dev-shortcuts for 0.20.3

--- app/main/main.js
+++ app/main/main.js
@@ -57,7 +57,7 @@ if (settings.cli) {
         launcher
 
     app.on('ready',function(){
-        launcher = require('./electron-window')({address:address, shortcuts:true, width:680, height:568, color:'#283143'})
+        launcher = require('./electron-window')({address:address, shortcuts:false, width:680, height:568, color:'#283143'})
     })
 
     process.stdout.write = (function(write) {
