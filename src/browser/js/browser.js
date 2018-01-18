b8a723daaf54d1f678fb2e92f1c3c3d809aee30d
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2018-01-18 15:13:02 +0100

    js: better loading order

--- src/browser/js/browser.js
+++ src/browser/js/browser.js
@@ -1,21 +1,28 @@
-
 require('./app/globals')
 require('./app/sourcemap')
 
-var ipc = require('./app/ipc/'),
-    osc = require('./app/osc'),
-    parser = require('./app/parser'),
-    {loading} = require('./app/ui/utils')
-
-ipc.init()
-osc.init()
-parser.init()
+var {loading} = require('./app/ui/utils')
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
     DOM.init()
+
     LOADING = loading('Connecting server...')
-    require('./app/ui/init')
-    ipc.send('ready')
+
+    setTimeout(()=>{
+
+        var ipc = require('./app/ipc/'),
+            osc = require('./app/osc'),
+            parser = require('./app/parser')
+
+        ipc.init()
+        osc.init()
+        parser.init()
+
+        require('./app/ui/init')
+        ipc.send('ready')
+
+
+    }, 50)
 
 })
