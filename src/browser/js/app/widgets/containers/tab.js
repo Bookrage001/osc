1b7f6f14a890d46dcb1387cfc354759e3236a077
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2018-02-15 09:50:34 +0100

    tab: dynamic label

--- src/browser/js/app/widgets/containers/tab.js
+++ src/browser/js/app/widgets/containers/tab.js
@@ -77,6 +77,7 @@ module.exports = class Tab extends Panel {
 
         switch (propName) {
 
+            case 'label':
             case 'color':
                 if (this.parent.createNavigation) this.parent.createNavigation()
                 return
