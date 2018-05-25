195f266c56ab0dd2ee0a25657026ef6993c61d48
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2018-05-25 13:43:07 +0200

    udp: error logging in udp module

--- app/main/osc/udp.js
+++ app/main/osc/udp.js
@@ -10,6 +10,10 @@ var oscUDPServer = new osc.UDPPort({
 	broadcast: true
 })
 
+oscUDPServer.on('error', function(error) {
+    console.error(error)
+})
+
 zeroconf.publish({
 	name: settings.read('appName') + (settings.read('instanceName') ? ' (' + settings.read('instanceName') + ')' : ''),
 	protocol: 'udp',
