2d278f1cf5690bb30d69d59245994bef5bc8320b
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2018-08-27 17:16:18 +0200

    locales: fr

---
+++ src/browser/js/app/locales/fr.js
@@ -0,0 +1,60 @@
+module.exports = {
+
+    // editor
+    editor_on: 'On',
+    editor_off: 'Off',
+    editor_root: 'Root',
+    editor_load: 'Charger',
+    editor_save: 'Sauver',
+
+    // editor context-menu
+    editor_editparent: 'Editer parent',
+    editor_copy: 'Copier',
+    editor_cut: 'Couper',
+    editor_wrap: 'Grouper dans',
+    editor_paste: 'Coller',
+    editor_pasteindent: 'ID + 1',
+    editor_clone: 'Clone',
+    editor_addwidget: 'Nouveau widget',
+    editor_addtab: 'Nouvel onglet',
+    editor_delete: 'Supprimer',
+
+    // loadings
+    loading_upload: 'Téléversement...',
+    loading_newclient: 'Connexion d\'un nouveau client...',
+    loading_server: 'Connexion au serveur...',
+
+    // fullscreen error
+    fs_unnavailable: 'Plein écran non disponible',
+    fs_addtohome: 'Vous devez ajouter cette page à votre écran d\'accueil pour pouvoir l\'ouvrir en plein écran.',
+
+    // session
+    session_browse: 'Parcourir',
+    session_new: 'Nouveau',
+    session_malformed: 'Fichier de session mal formé',
+    session_parsingerror: 'Erreur à l\'ouverture de la session',
+    session_uploaderror: 'Echec à l\'envoi du fichier de session.',
+
+    // sidepanel
+    sidepanel_fs: 'Plein Écran',
+    sidepanel_state: 'État',
+    sidepanel_traversing: 'Gestes traversants',
+    sidepanel_editor: 'Éditeur',
+
+    // state
+    state_store: 'Sauver',
+    state_recall: 'Rappeler',
+    state_send: 'Envoyer',
+    state_import: 'Importer',
+    state_export: 'Exporter',
+    state_uploaderror: 'Echec à l\'envoi du fichier d\'état.',
+
+    // traversing
+    traversing_on: 'On',
+    traversing_off: 'Off',
+
+    // misc
+    error: 'Erreur'
+
+
+}
