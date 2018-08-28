a345114fbdcd24431401c1efe1e7aaa9d5471f21
bordun <jean-emmanuel.doucet@groolot.net>
2018-08-28 14:15:42 +0200

    ios: pulltorefresh: never prevent 1st move on draggable widgets

--- src/browser/js/app/ui/ios.js
+++ src/browser/js/app/ui/ios.js
@@ -5,7 +5,7 @@ if (!!navigator.platform.match(/iPhone|iPod|iPad/)) {
     var preventNextMove = false
 
     document.addEventListener('touchstart', (e)=>{
-        if (e.touches.length === 1) preventNextMove = (window.pageYOffset === 0)
+        if (e.touches.length === 1 && !e.target._drag_widget) preventNextMove = (window.pageYOffset === 0)
     }, false)
 
     document.addEventListener('touchmove', (e)=>{
