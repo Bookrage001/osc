d125030a9966cee4130d0cd60ff6f5e15ac0ee41
bordun <jean-emmanuel.doucet@groolot.net>
2018-10-26 20:11:10 +0200

    travis: fix package all

--- scripts/package.js
+++ scripts/package.js
@@ -3,11 +3,13 @@ const packager = require('electron-packager'),
       appData = require('../app/package.json'),
       safeFFMPEG = require('electron-packager-plugin-non-proprietary-codecs-ffmpeg').default
 
-var rpi = process.argv.includes('--rpi')
+var rpi = process.argv.includes('--rpi'),
+    all = process.argv.includes('--all')
 
 packager({
     dir: path.resolve(__dirname + '/../app'),
     name: appData.name,
+    all: all ? true : undefined,
     platform: rpi ? 'linux' : process.env.PLATFORM,
     arch: rpi ? 'armv7l' : process.env.ARCH,
     electronVersion:  rpi ? '1.7.11' : undefined,
