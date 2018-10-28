0ce4af3275c182c5d61cc264b0152ba5bd42e095
bordun <jean-emmanuel.doucet@groolot.net>
2018-10-28 16:00:59 +0100

    package: create app/package.json at build time

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -1,4 +1,5 @@
 var cpr = require('cpr'),
+    fs = require('fs'),
     path = require('path'),
     files = [
         ['../resources/images/logo-nobadge.png', '../app/assets/favicon.png'],
@@ -9,3 +10,31 @@ var cpr = require('cpr'),
 for (var i in files) {
     cpr(...files[i].map(f => path.resolve(__dirname + '/' + f)), {})
 }
+
+
+var packageJson = require('../package.json'),
+    appJson = {},
+    copiedProps = [
+        "name",
+        "productName",
+        "description",
+        "version",
+        "author",
+        "repository",
+        "homepage",
+        "license",
+        "yargs",
+        "engines"
+    ]
+
+for (var k of copiedProps) {
+    appJson[k] = packageJson[k]
+}
+
+appJson.main = appJson.bin = "index.js"
+appJson.scripts = {
+  "start": "electron index.js",
+  "start-node": "node index.js"
+}
+
+fs.writeFileSync(path.resolve(__dirname + '/../app/package.json'), JSON.stringify(appJson, null, '  '))
