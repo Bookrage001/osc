55487fd1e13ad2deb1c750174c2a3f09d1022cbd
bordun <jean-emmanuel.doucet@groolot.net>
2018-10-29 10:47:14 +0100

    package: refix client bundle bize while keeping --watch error log

--- scripts/build.js
+++ scripts/build.js
@@ -35,8 +35,8 @@ module.exports = (opt) => {
 
         var output =  b.bundle()
 
-        output.pipe(exorcist(outputPath + '.map'))
-        output.pipe(fs.createWriteStream(outputPath))
+        var toWrite = output.pipe(exorcist(outputPath + '.map'))
+        toWrite.pipe(fs.createWriteStream(outputPath))
 
         return output
 
