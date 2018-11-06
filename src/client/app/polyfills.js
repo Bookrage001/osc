1db3b3b7d18f2b4c8654c60d14f1d5554832629c
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2018-11-06 20:32:45 +0100

    dom: poylfill HTMLCollection foreach

--- src/client/app/polyfills.js
+++ src/client/app/polyfills.js
@@ -8,5 +8,13 @@ if (!Object.values) {
 }
 
 if (!NodeList.prototype.forEach) {
+
     NodeList.prototype.forEach = Array.prototype.forEach
+
+}
+
+if (!HTMLCollection.prototype.forEach) {
+
+    HTMLCollection.prototype.forEach = Array.prototype.forEach
+
 }
