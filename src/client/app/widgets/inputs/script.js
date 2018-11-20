af3e0943169ff45babea854f0cc0e65528ead601
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2018-11-20 19:58:34 +0100

    docs: mention how scripts can receive other widgets' values (#423)

--- src/client/app/widgets/inputs/script.js
+++ src/client/app/widgets/inputs/script.js
@@ -18,10 +18,11 @@ class Script extends Widget {
                 '- `value`: the value received by the widget'
             ]},
             script: {type: 'string', value: '', help: [
-                'This property is evaluated each time the widget receives a value if condition is non-falsy. Formulas are given extras variables in this context:',
+                'This property is evaluated each time the widget receives a value* if condition is non-falsy. Formulas are given extras variables in this context:',
                 '- `value`: the value received by the widget',
                 '- `send(target, address, arg1, arg2, ...)`: function for sending osc messages (ignores the script\'s targets and the server\'s defaults unless `target` is `false`; ignores the script\'s `preArgs`)',
-                '- `set(id, value)`: function for setting a widget\'s value'
+                '- `set(id, value)`: function for setting a widget\'s value',
+                '', '* Note: `value` or `linkId` can be used to receive other widgets\' values'
             ]}
 
         })
