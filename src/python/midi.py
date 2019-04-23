9b5e9fec020ea4ca1df8c795758f31c9c7f2f336
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2019-04-23 22:53:45 +0200

    midi: fix error msg for invalid address

--- src/python/midi.py
+++ src/python/midi.py
@@ -133,7 +133,7 @@ def send_midi(name, event, *args):
         return
 
     if event not in OSC_TO_MIDI:
-        ipc_send('log','ERROR: MIDI: unknown output (%s)' % name)
+        ipc_send('log','ERROR: MIDI: invalid address (%s)' % event )
         return
 
     m = None
